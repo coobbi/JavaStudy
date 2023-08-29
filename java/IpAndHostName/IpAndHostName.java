22795dd134309cfd7813b5578b66248d75c9c697
coobbi <138000094+coobbi@users.noreply.github.com>
2023-08-29 17:02:40 +0800

    Create IpAndHostName.java

---
+++ java/IpAndHostName/IpAndHostName.java
@@ -0,0 +1,11 @@
+import java.net.InetAddress;
+ 
+public class IpAndHostName {
+   public static void main(String[] args) 
+   throws Exception {
+      InetAddress addr = InetAddress.getLocalHost();
+      System.out.println("Local HostAddress: " + addr.getHostAddress());
+      String hostname = addr.getHostName();
+      System.out.println("Local host name: "+ hostname);
+   }
+}
