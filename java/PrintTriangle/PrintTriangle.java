9fec32711291ee9b5b077c5857b305154da23693
coobbi <138000094+coobbi@users.noreply.github.com>
2023-08-29 17:01:33 +0800

    Create PrintTriangle.java

---
+++ java/PrintTriangle/PrintTriangle.java
@@ -0,0 +1,13 @@
+class PrintTriangle {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            for(int j=5; i<=j; j--)
+                System.out.print(" ");
+            for(int j=1; j<=i; j++)
+                System.out.print("*");
+            for(int j=1; j<i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
